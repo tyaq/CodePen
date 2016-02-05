6f326be2f529e14e18d960490c91b0ed2e82da57
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2016-02-04 22:11:03 -0500

    Make Background Color update with time
    Created a color function

--- color-clock/js/index.js
+++ color-clock/js/index.js
@@ -1,11 +1,18 @@
 function tickTock() {
   var time, hrs, min, sec;
+  var background, shadow;
 
   // gets the hours, min, & sec
   time = new Date();
   hrs = time.getHours();
   min = time.getMinutes();
   sec = time.getSeconds();
+  var t = (hrs * 60 + min) * 60 + sec;
+
+  var S = 255 - 15 * (Math.cos(2 * Math.PI * sec / 60) + 1);
+
+  background = getColor(255, t);
+  shadow = getColor(S, t);
 
   //Pads single digits with leading zeros
   if (min <= 9) {
@@ -20,12 +27,57 @@ function tickTock() {
   var hrs = hrs % 12;
   // the 24-hour '0' will be '12'
   hrs = hrs ? hrs : 12;
-
-  //Displays time
+  var S = "-webkit-linear-gradient(" + background + "," + shadow + ")";
+  S = "linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet)"
+    //Displays time
   $('#clock').text(hrs + ":" + min);
   $('#meridiem').text(sec + " " + meridiem);
+  $('#colorCode').text(background);
+  $('body').css("background-color", background);
 
 };
+
+function getColor(A, t) {
+  var r, b, g;
+  var div = 24 * 60 * 60 / 6;
+
+  if (t <= div) { //4
+    b = A;
+    g = A * t / div;
+    r = 0;
+  } else if (t <= 2 * div) { //8
+    b = A - A * (t - div) / div;
+    g = A;
+    r = 0;
+  } else if (t <= 3 * div) { //12
+    b = 0;
+    g = A;
+    r = A * (t - 2 * div) / div;
+  } else if (t <= 4 * div) { //16
+    b = 0;
+    g = A - A * (t - 3 * div) / div;
+    r = A;
+  } else if (t <= 5 * div) { //20
+    b = A * (t - 4 * div) / div;
+    g = 0;
+    r = A;
+  } else if (t <= 6 * div) { //24
+    b = A;
+    g = 0;
+    r = A - A * (t - 5 * div) / div;
+  };
+
+  var rbg = "rgb(" + r + "," + g + "," + b + ")";
+  var hex = "#";
+  r = Math.round(r);
+  g = Math.round(g);
+  b = Math.round(b);
+  r == 0 ? hex += '00' : hex += r.toString(16);
+  g == 0 ? hex += '00' : hex += g.toString(16);
+  b == 0 ? hex += '00' : hex += b.toString(16);
+
+  return hex.toUpperCase();
+}
 
 $(document).ready(function() {
 
